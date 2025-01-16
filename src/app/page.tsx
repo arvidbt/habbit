f45e712f58034ffcf9cea325dbabd13d5a0e4c4d
arvidbt <arvid.bergman.thorn@cygni.se>
2025-01-16 13:48:54 +0100

    update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { HydrateClient } from '@/trpc/server'
 import { SiteHeader } from '@/components/site-header'
 import { auth } from '@/server/auth'
+import { FloatingNavbar } from '@/components/floating-navbar'
 
 export default async function Home() {
     const session = await auth()
-
-    console.log(session)
     return (
         <HydrateClient>
             <main className="flex w-full flex-col items-center">
                 <SiteHeader />
+                {session && <FloatingNavbar />}
             </main>
         </HydrateClient>
     )
