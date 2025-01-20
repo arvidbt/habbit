0d2dbab6b3a77cc333f29012f0346621c92b600f
Albin Frick <abbzez@gmail.com>
2025-01-21 00:20:09 +0100

    fix: botched merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { HydrateClient } from '@/trpc/server'
 import { SiteHeader } from '@/components/site-header'
 import { auth } from '@/server/auth'
 import { FloatingNavbar } from '@/components/floating-navbar'
+import { type Habit } from '@/types/habit'
+import { HabitCard } from '@/components/HabitCard'
 
 export default async function Home() {
   const session = await auth()
