8be68929c27c6c5eb4123c5e956b4ff871154608
Albin Frick <abbzez@gmail.com>
2025-01-21 14:55:44 +0100

    ref: correct naming

--- src/server/api/routers/habit.ts
+++ src/server/api/routers/habit.ts
@@ -5,28 +5,27 @@ import { habits } from '@/server/db/schema'
 import { eq } from 'drizzle-orm'
 
 export const habitRouter = createTRPCRouter({
-    create: protectedProcedure
-        .input(
-            z.object({
-                what: z.string().min(1),
-                why: z.string().min(1),
-                when: z.string().min(1),
-            })
-        )
-        .mutation(async ({ ctx, input }) => {
-            await ctx.db.insert(habits).values({
-                createdById: ctx.session.user.id,
-                what: input.what,
-                why: input.why,
-                when: input.when,
-            })
-        }),
-
-    getHabits: protectedProcedure.query(async ({ ctx }) => {
-        const post = await ctx.db.query.habits.findMany({
-            where: eq(habits.createdById, ctx.session.user.id),
-        })
-
-        return post ?? null
+  create: protectedProcedure
+    .input(
+      z.object({
+        what: z.string().min(1),
+        why: z.string().min(1),
+        when: z.string().min(1),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(habits).values({
+        createdById: ctx.session.user.id,
+        what: input.what,
+        why: input.why,
+        when: input.when,
+      })
     }),
+
+  getHabits: protectedProcedure.query(async ({ ctx }) => {
+    const fetchedHabits = await ctx.db.query.habits.findMany({
+      where: eq(habits.createdById, ctx.session.user.id),
+    })
+    return fetchedHabits ?? null
+  }),
 })
