36a213da5f00fcae09354c1f7073f7a97628ee2e
Albin Frick <abbzez@gmail.com>
2025-01-21 14:34:01 +0100

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,22 @@ import { HydrateClient } from '@/trpc/server'
 import { SiteHeader } from '@/components/site-header'
 import { auth } from '@/server/auth'
 import { FloatingNavbar } from '@/components/floating-navbar'
+import { type Habit } from '@/types/habit'
+import { HabitCard } from '@/components/habit-card'
 
 export default async function Home() {
   const session = await auth()
+  const habit: Habit = {
+    habit: 'fixa mitt tangentbord',
+    when: 'nu',
+    why: 'Så att jag kan skriva kod and make money',
+    completed: [],
+  }
   return (
     <HydrateClient>
       <main className="flex w-full flex-col items-center">
         <SiteHeader />
+        <HabitCard habit={habit} />
         {session && <FloatingNavbar />}
       </main>
     </HydrateClient>
