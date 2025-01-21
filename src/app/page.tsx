a088c288f67f4e464d05d8382d4fd105f3576b8b
Albin Frick <abbzez@gmail.com>
2025-01-21 10:52:27 +0100

    format and adds posthog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { auth } from '@/server/auth'
 import { FloatingNavbar } from '@/components/floating-navbar'
 
 export default async function Home() {
-    const session = await auth()
-    return (
-        <HydrateClient>
-            <main className="flex w-full flex-col items-center">
-                <SiteHeader />
-                {session && <FloatingNavbar />}
-            </main>
-        </HydrateClient>
-    )
+  const session = await auth()
+  return (
+    <HydrateClient>
+      <main className="flex w-full flex-col items-center">
+        <SiteHeader />
+        {session && <FloatingNavbar />}
+      </main>
+    </HydrateClient>
+  )
 }
--- src/components/create-habit-drawer.tsx
+++ src/components/create-habit-drawer.tsx
@@ -1,33 +1,31 @@
 import {
-    Drawer,
-    DrawerClose,
-    DrawerContent,
-    DrawerDescription,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
 } from '@/components/ui/drawer'
 import { Button } from './ui/button'
 import { Icons } from './icons'
 import { CreateHabitForm } from './create-habit-form'
 
 export async function CreateHabitDrawer() {
-    return (
-        <Drawer>
-            <DrawerTrigger asChild>
-                <Button size={'default'}>
-                    <Icons.SquarePlus />
-                </Button>
-            </DrawerTrigger>
-            <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle></DrawerTitle>
-                </DrawerHeader>
-                <DrawerFooter>
-                    <CreateHabitForm />
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
-    )
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button size={'default'}>
+          <Icons.SquarePlus />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle></DrawerTitle>
+        </DrawerHeader>
+        <DrawerFooter>
+          <CreateHabitForm />
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
 }
