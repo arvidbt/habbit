2cf25a8b25ce5a4046517434bb7bbfbdd370eafb
Albin Frick <abbzez@gmail.com>
2025-01-22 11:29:34 +0100

    ref: remove unused habit call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { api, HydrateClient } from '@/trpc/server'
+import { HydrateClient } from '@/trpc/server'
 import { SiteHeader } from '@/components/site-header'
 import { auth } from '@/server/auth'
 import { FloatingNavbar } from '@/components/floating-navbar'
@@ -6,8 +6,6 @@ import { HabitGrid } from '@/components/habit-grid'
 
 export default async function Home() {
   const session = await auth()
-  const habit = await api.habit.getHabits()
-  console.log(habit)
 
   return (
     <HydrateClient>
