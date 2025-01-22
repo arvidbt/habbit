4c364753fda020decbe492c75cee21a1ce9d612b
Albin Frick <abbzez@gmail.com>
2025-01-22 11:30:14 +0100

    style: style fix

--- src/components/habit-grid.tsx
+++ src/components/habit-grid.tsx
@@ -1,18 +1,15 @@
 'use client'
-
 import React from 'react'
-import { api } from '@/trpc/react'
 import { HabitCard } from './habit-card'
+import { api } from '@/trpc/react'
 
 export const HabitGrid = () => {
   const habits = api.habit.getHabits.useQuery().data
-  if (!habits) {
-    return
-  }
 
-  console.log(habits)
+  if (!habits) return <p>Click plus button to add habits</p>
+
   return (
-    <div className="flex h-screen w-full flex-col items-center gap-6">
+    <div className="flex min-h-screen w-full flex-col items-center gap-6">
       {habits.map((habit, i) => {
         if (!habit) {
           return <p key={i}>no habitsius</p>
