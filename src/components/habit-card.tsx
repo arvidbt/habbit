754854089c7d1fe28f97377f29e666d1788c15b5
Albin Frick <abbzez@gmail.com>
2025-01-23 17:03:21 +0100

    feat: use habit form to update and delete a habit

--- src/components/habit-card.tsx
+++ src/components/habit-card.tsx
@@ -11,16 +11,7 @@ import { cn } from '@/lib/utils'
 import { type Habit } from '@/server/api/routers/habit'
 import posthog from 'posthog-js'
 import { Icons } from './icons'
-import {
-  Drawer,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from './ui/drawer'
-import { CreateHabitForm } from './create-habit-form'
-import { Button } from './ui/button'
+import { HabitForm } from './habit-form'
 
 interface HabitCardProps {
   habit: Habit
@@ -30,6 +21,7 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
   const [scope, animate] = useAnimate()
   const [holdTimeout, setHoldTimeout] = useState<NodeJS.Timeout | null>(null)
   const [isCompleted, setIsCompleted] = useState(false)
+  const [isEditing, setIsEditing] = useState(false)
 
   const completedAnimation = () => {
     animate(
@@ -99,102 +91,106 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
   }
 
   return (
-    <Drawer>
+    <div
+      ref={scope}
+      className="relative h-[80dvh] w-full max-w-[400px] overflow-clip rounded-3xl bg-white shadow-lg"
+    >
       <div
-        ref={scope}
-        className="relative h-[80dvh] w-full max-w-[400px] overflow-clip rounded-3xl bg-white shadow-lg"
+        id="count"
+        className={cn(
+          isCompleted ? 'from-sapphire to-green' : 'from-sky to-blue',
+          'absolute right-2 top-2 z-50 flex items-center gap-1 rounded-xl bg-peach bg-gradient-to-r px-3 py-1.5'
+        )}
       >
-        <div
-          id="count"
-          className={cn(
-            isCompleted ? 'from-sapphire to-green' : 'from-sky to-blue',
-            'absolute right-2 top-2 z-50 flex items-center gap-1 rounded-xl bg-peach bg-gradient-to-r px-3 py-1.5'
-          )}
-        >
-          <Icons.Zap className="size-6" />
-          <p className="text-xl">1</p>
-        </div>
-
-        <span
-          id="backdrop"
-          className="absolute bottom-[25%] left-1/2 h-0 -translate-x-1/2 translate-y-1/2 rounded-full"
-        ></span>
-
-        <div
-          id="content"
-          className="justify absolute inset-0 flex max-w-md flex-col items-center justify-between px-6 pb-16 shadow-lg md:px-16"
-        >
-          <div className="mt-2 flex flex-col items-center gap-8">
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <DrawerTrigger asChild>
-                    <button
-                      onClick={() => {
-                        console.log('open habit editor')
-                      }}
-                    >
-                      <Icons.Ellipsis className="text-text" />
-                    </button>
-                  </DrawerTrigger>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p>Redigera vana</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <div id="habit-text-container" className="space-y-4 text-text">
-              <motion.p
-                initial={{ opacity: 0, x: -200 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ ease: [0, 0.71, 0.2, 1.01], duration: 0.5 }}
-                className="font-serif text-4xl font-bold"
-              >
-                {habit.what}
-              </motion.p>
-              <motion.p
-                initial={{ opacity: 0, x: 200 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ ease: [0, 0.71, 0.2, 1.01], duration: 0.5 }}
-                className="text-xl"
-              >
-                {habit.when}
-              </motion.p>
-              <motion.p
-                initial={{ opacity: 0, x: -200 }}
-                animate={{ opacity: 1, x: 0 }}
-                transition={{ ease: [0, 0.71, 0.2, 1.01], duration: 0.5 }}
-                className="text-2xl"
-              >
-                {habit.why}
-              </motion.p>
-            </div>
-          </div>
-
-          <motion.button
-            onContextMenu={(e) => e.preventDefault()}
-            disabled={isCompleted}
-            id="check-button"
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ delay: 0.5 }}
-            onTapStart={() => !isCompleted && handleHoldStart()}
-            onTap={handleHoldEnd}
-            onTapCancel={handleHoldEnd}
-            className="rounded-full bg-base p-12 text-text"
-          >
-            <Icons.Check className="size-24" />
-          </motion.button>
-        </div>
+        <Icons.Zap className="size-6" />
+        <p className="text-xl">1</p>
       </div>
-      <DrawerContent>
-        <DrawerHeader>
-          <DrawerTitle></DrawerTitle>
-        </DrawerHeader>
-        <DrawerFooter>
-          <CreateHabitForm />
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
+
+      <span
+        id="backdrop"
+        className="absolute bottom-[25%] left-1/2 h-0 -translate-x-1/2 translate-y-1/2 rounded-full"
+      ></span>
+
+      <div
+        id="content"
+        className="justify absolute inset-0 flex max-w-md flex-col items-center justify-between px-6 pb-16 shadow-lg md:px-16"
+      >
+        <div className="mt-2 flex flex-col items-center gap-8">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <button
+                  onClick={() => {
+                    setIsEditing(!isEditing)
+                  }}
+                >
+                  <Icons.Ellipsis className="text-text" />
+                </button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Redigera vana</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <div id="habit-text-container" className="space-y-4 text-text">
+            {isEditing ? (
+              <HabitForm habit={habit} onSuccess={() => setIsEditing(false)} />
+            ) : (
+              <HabitText habit={habit} />
+            )}
+          </div>
+        </div>
+
+        <motion.button
+          onContextMenu={(e) => e.preventDefault()}
+          disabled={isCompleted}
+          id="check-button"
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{ delay: 0.5 }}
+          onTapStart={() => !isCompleted && handleHoldStart()}
+          onTap={handleHoldEnd}
+          onTapCancel={handleHoldEnd}
+          className="rounded-full bg-base p-12 text-text"
+        >
+          <Icons.Check className="size-24" />
+        </motion.button>
+      </div>
+    </div>
+  )
+}
+
+const HabitText = ({ habit }: { habit: Habit }) => {
+  const capitalizeFirst = (text: string) => {
+    return text.charAt(0).toUpperCase() + text.slice(1)
+  }
+  return (
+    <>
+      <motion.p
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ ease: [0.2, 0.71, 0.2, 1.01], duration: 0.5 }}
+        className="font-serif text-4xl font-bold"
+      >
+        {capitalizeFirst(habit.what)}
+      </motion.p>
+      <motion.p
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ ease: [0.2, 0.71, 0.2, 1.01], duration: 0.3, delay: 0.1 }}
+        className="text-xl"
+      >
+        {capitalizeFirst(habit.when)}
+      </motion.p>
+      <p className="italic text-subtext0">so I can</p>
+      <motion.p
+        initial={{ opacity: 0, scale: 0.9 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ ease: [0.2, 0.71, 0.2, 1.01], duration: 0.3, delay: 0.2 }}
+        className="text-2xl"
+      >
+        {capitalizeFirst(habit.why)}
+      </motion.p>
+    </>
   )
 }
