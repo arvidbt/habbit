b78958965e7195c58d5f18dcfb5b6e5288ad8a95
arvidbt <arvid.bergman.thorn@cygni.se>
2025-01-23 12:48:57 +0100

    add cl

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ['prettier-plugin-tailwindcss'],
-  trailingComma: 'es5',
-  tabWidth: 2,
-  semi: false,
-  singleQuote: true,
+const config = {
+    plugins: ['prettier-plugin-tailwindcss'],
+    trailingComma: 'es5',
+    tabWidth: 2,
+    semi: false,
+    singleQuote: true,
 }
+export default config
