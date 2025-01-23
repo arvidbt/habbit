f2bc2b06b921f3c1618809619320a9134f6c38b9
arvidbt <arvid.bergman.thorn@cygni.se>
2025-01-23 12:55:53 +0100

    wrap habit grid in session check to avoid trpc errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,15 +5,15 @@ import { FloatingNavbar } from '@/components/floating-navbar'
 import { HabitGrid } from '@/components/habit-grid'
 
 export default async function Home() {
-  const session = await auth()
+    const session = await auth()
 
-  return (
-    <HydrateClient>
-      <main className="flex w-full flex-col items-center">
-        <SiteHeader />
-        <HabitGrid />
-        {session && <FloatingNavbar />}
-      </main>
-    </HydrateClient>
-  )
+    return (
+        <HydrateClient>
+            <main className="flex w-full flex-col items-center">
+                <SiteHeader />
+                {session && <HabitGrid />}
+                {session && <FloatingNavbar />}
+            </main>
+        </HydrateClient>
+    )
 }
