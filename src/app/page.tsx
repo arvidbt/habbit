688ec0892d555a922c8866d742a7730249fe1f41
Albin Frick <abbzez@gmail.com>
2025-01-27 21:35:53 +0100

    adds demo habit on startpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { HabitGrid } from '@/components/habit-grid'
 import { Button } from '@/components/ui/button'
 import { SignInButton } from '@/components/sign-in-button'
 import { Icons } from '@/components/icons'
+import { HabitCard } from '@/components/habit-card'
+import { Habit } from '@/server/api/routers/habit'
 
 export default async function Home() {
   const session = await auth()
@@ -26,6 +28,16 @@ export default async function Home() {
 }
 
 function NotLoggedInStartPage() {
+  const demoHabit: Habit = {
+    what: 'Walk the dog',
+    why: 'To exercise',
+    when: 'Every day',
+    createdById: '1',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    id: 0
+  }
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 w-full min-h-[calc(100dvh-48px)] items-center">
       <div className='mx-auto space-y-18'>
@@ -37,7 +49,9 @@ function NotLoggedInStartPage() {
           Get started
         </SignInButton >
       </div>
-      <div className='border border-2 border-subtext0 rounded-4xl w-[380px] h-[680px] mx-auto'>
+      <div className='border border-2 border-subtext0 rounded-4xl flex flex-col gap-6 p-11 w-[380px] h-[680px] mx-auto'>
+        <h2 className='text-text text-2xl font-bold'>Your habits</h2>
+        <HabitCard habit={demoHabit} demo />
       </div>
     </div>
   )
