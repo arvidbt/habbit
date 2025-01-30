2234b7ad3e3772fd2ee85fe6f2f7576db88f1ce0
Albin Frick <albinfrick@proton.me>
2025-01-30 08:50:46 +0100

    Feature/bnk 36 create start page (#46)
    * feat: add rabbit icon
    
    * adds a little debugg helping for overflow elements
    
    * fix: make sign in button more general
    
    * wip: start page design
    
    * adds demo habit on startpage
    
    * style: phone frame for the demo
    
    * ref: fix formatting
    
    * formatting
    
    * wip setting up solid mobile + desktop layout
    
    * fix: disable buttons in form when habit is a demo
    
    * style: fix start page

--- src/components/habit-card.tsx
+++ src/components/habit-card.tsx
@@ -15,9 +15,10 @@ import { HabitForm } from './habit-form'
 
 interface HabitCardProps {
   habit: Habit
+  demo?: boolean
 }
 
-export const HabitCard = ({ habit }: HabitCardProps) => {
+export const HabitCard = ({ habit, demo }: HabitCardProps) => {
   const [scope, animate] = useAnimate()
   const [holdTimeout, setHoldTimeout] = useState<NodeJS.Timeout | null>(null)
   const [isCompleted, setIsCompleted] = useState(false)
@@ -28,7 +29,7 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
       '#backdrop',
       {
         background:
-          ' linear-gradient(61deg, rgba(0,18,36,0.8393951330532212) 0%, rgba(17,185,35,1) 35%, rgba(0,255,186,1) 100%)',
+          ' linear-gradient(61deg, rgba(0,18,36,0.8393951330532212) 0%, rgba(17,185,35,1) 35%, rgba(0,255,186,0.4) 100%)',
         opacity: 1,
         height: '175%',
         aspectRatio: 1 / 1,
@@ -68,11 +69,7 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
       { scale: 1, background: 'rgba(var(--ctp-base),1)' },
       { duration: 0.8 }
     )
-    animate(
-      '#count',
-      { rotate: 0, scale: 1 },
-      { duration: 0.3 }
-    )
+    animate('#count', { rotate: 0, scale: 1 }, { duration: 0.3 })
   }
 
   const handleHoldStart = () => {
@@ -98,10 +95,13 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
   return (
     <motion.div
       ref={scope}
-      className="relative h-[80dvh] max-h-[40rem] w-full max-w-[36rem] overflow-clip rounded-3xl bg-white shadow-lg"
       initial={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.3 }}
+      className={cn(
+        'relative h-[80dvh] w-full max-w-[400px] overflow-clip rounded-3xl bg-white shadow-lg',
+        demo && 'h-full'
+      )}
     >
       <div
         id="count"
@@ -116,44 +116,54 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
 
       <span
         id="backdrop"
-        className="absolute bottom-[25%] left-1/2 h-0 -translate-x-1/2 translate-y-1/2 rounded-full"
+        className={cn(
+          'absolute bottom-[25%] left-1/2 h-0 -translate-x-1/2 translate-y-1/2 rounded-full',
+          demo && 'bottom-[20%]'
+        )}
       ></span>
 
       <div
         id="content"
-        className="justify absolute inset-0 z-50 flex flex-col items-center justify-between px-6 pb-16 shadow-lg md:px-16"
+        className={cn(
+          'justify absolute inset-0 flex max-w-md flex-col items-center justify-between px-6 pb-16 shadow-lg md:px-16',
+          demo && 'pb-8'
+        )}
       >
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <button
-                className='mt-2'
-                  onClick={() => {
-                    setIsEditing(!isEditing)
-                  }}
-                >
-                  <Icons.Ellipsis className="text-text" />
-                </button>
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>Redigera vana</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-          <div id="habit-text-container" className="text-text space-y-4">
-            {isEditing ? (
-              <motion.div
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <button
+                className="mt-2"
+                onClick={() => {
+                  setIsEditing(!isEditing)
+                }}
+              >
+                <Icons.Ellipsis className="text-text" />
+              </button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Redigera vana</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+        <div id="habit-text-container" className="text-text space-y-4">
+          {isEditing ? (
+            <motion.div
               initial={{ opacity: 0, y: -10 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.3 }}
-              exit={{ opacity: 0, y: -20 }} 
-              >
-              <HabitForm habit={habit} onSuccess={() => setIsEditing(false)} />
-              </motion.div>
-            ) : (
-              <HabitText habit={habit} />
-            )}
-          </div>
+              exit={{ opacity: 0, y: -20 }}
+            >
+              <HabitForm
+                demo={demo}
+                habit={habit}
+                onSuccess={() => setIsEditing(false)}
+              />
+            </motion.div>
+          ) : (
+            <HabitText habit={habit} />
+          )}
+        </div>
 
         <motion.button
           onContextMenu={(e) => e.preventDefault()}
@@ -165,9 +175,9 @@ export const HabitCard = ({ habit }: HabitCardProps) => {
           initial={{ scale: 0 }}
           animate={{ scale: 1 }}
           transition={{ delay: 0.5 }}
-          className="text-text bg-base rounded-full p-12"
+          className={cn('text-text bg-base rounded-full p-12', demo && 'p-10')}
         >
-          <Icons.Check className="size-24" />
+          <Icons.Check className={cn('size-24', demo && 'size-18')} />
         </motion.button>
       </div>
     </motion.div>
