e472bda308c1f3f01a9f2e3f913b214ee69fc7b9
Albin Frick <albinfrick@proton.me>
2025-02-03 12:00:59 +0100

    style: fix spacing and font sizes for start page (#52)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,19 +42,19 @@ function NotLoggedInStartPage() {
   return (
     <div className="flex min-h-[calc(100dvh-48px)] w-full flex-col items-center justify-between px-6 md:flex-row">
       <div className="flex flex-col items-center gap-10 py-11 md:items-start md:gap-18">
-        <div className="flex flex-col items-center gap-4 md:items-start md:gap-0">
-          <h1 className="text-text flex gap-4 font-serif text-8xl font-bold">
+        <div className="flex flex-col items-center gap-3 md:items-start md:gap-0">
+          <h1 className="text-text flex gap-4 font-serif text-7xl font-bold md:text-8xl">
             Habbit <Icons.Rabbit className="text-peach size-18 -rotate-12" />
           </h1>
-          <p className="text-subtext0 text-2xl">
+          <p className="text-subtext0 text-xl md:text-2xl">
             Take <span className="text-mauve">control</span> over your life
           </p>
         </div>
         <SignInButton size="lg">Get started</SignInButton>
       </div>
 
-      <div className="md:bg-subtext0 w-full max-w-md rounded-4xl bg-transparent p-3">
-        <div className="bg-base flex h-[680px] w-full flex-col gap-6 rounded-3xl p-11">
+      <div className="md:bg-subtext0 w-full max-w-md rounded-4xl bg-transparent p-0 md:p-3">
+        <div className="bg-base flex h-[680px] w-full flex-col gap-6 rounded-3xl p-2 md:p-11">
           <div>
             <h2 className="text-text text-2xl font-bold">Demo</h2>
             <p className="text-subtext0">
